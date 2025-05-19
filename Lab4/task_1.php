ef758ba9cf86256bf380e530a0d16ca5f2ffe48d
Artakusken <108093154+Artakusken@users.noreply.github.com>
2025-05-19 20:15:48 +0200

    Исправил task_1.php

--- Lab4/task_1.php
+++ Lab4/task_1.php
@@ -1,7 +1,14 @@
-$str = 'ahb acb aeb aeeb adcb axeb';
-$regexp = '/b...b/iu';
-$matches = [];
+<?php
+$str1 = 'ahb bacb aeb aeeb abdcb axeb';
+$regexp1 = '/b..b/iu';
+$super_regexp1 = '/(?=(b..b))/iu';
+$matches1 = [];
+$super_matches1 = [];
 
-$count = preg_match_all($regexp, $str, $matches);
+$count = preg_match_all($regexp1, $str1, $matches1);
 echo "number of substrings $count: \n";
-var_dump($matches);
+var_dump($matches1);
+$count = preg_match_all($super_regexp1, $str1, $super_matches1);
+echo "number of all substrings $count: \n";
+var_dump($super_matches1[1]);
+?>
